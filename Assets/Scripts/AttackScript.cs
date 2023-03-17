10723df54edaec4dd8c29ac147907bf97cd39d08
DanielBauge <joel.larsson3@elev.ga.lbs.se>
2023-03-17 08:57:31 +0100

    Test

---
+++ Assets/Scripts/AttackScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballScript : MonoBehaviour
+{
+    public float Damage;
+
+    void fixedUpdate()
+    {
+        if(Input.getButton(mouseButton.1))
+        {
+
+        }
+    }
+}
